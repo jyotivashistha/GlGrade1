b2804f546f8ffd4de69652082c9c09a34eca975b
jyotivashistha <87970130+jyotivashistha@users.noreply.github.com>
2021-09-05 17:45:07 +0530

    Update Driver.java

--- src/com/gl/superDepartment/Driver.java
+++ src/com/gl/superDepartment/Driver.java
@@ -9,41 +9,39 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Admin ad1=new Admin();
-		HrDepartment hr=new HrDepartment();
-		TechDepartment tech=new TechDepartment();
-		
-		
-		System.out.println("Enter any Department from the following");
-		System.out.println("1. Admin Deaprtment");
-		System.out.println("2. Hr Department");
-		System.out.println("3. Tech Department");
-		Scanner sc=new Scanner(System.in);
-		int No=sc.nextInt();
-		
-		
-		switch(No) {
-		case 1:
 		 System.out.println(ad1.departmentName());
 		 System.out.println(ad1.getTodaysWork());
 		 System.out.println(ad1.getWorkDeadline());
 		 System.out.println(ad1.isTodayAHoliday());
-		break;
-		case 2:
-			 System.out.println(hr.departmentName());
-			 System.out.println(hr.doActivity());
-			 System.out.println(hr.getTodaysWork());
-			 System.out.println(hr.getWorkDeadline());
-			 System.out.println(hr.isTodayAHoliday());
-		break;
-		case 3:
-			 System.out.println(tech.departmentName());
-			 System.out.println(tech.getTodaysWork());
-			 System.out.println(tech.getWorkDeadline());
-			 System.out.println(tech.getTechStackInformation());
-			 System.out.println(tech.isTodayAHoliday());
+		 System.out.println("==========================================================================");
+		 
+		 
+		 HrDepartment hr=new HrDepartment();
+		 System.out.println(hr.departmentName());
+		 System.out.println(hr.doActivity());
+		 System.out.println(hr.getTodaysWork());
+		 System.out.println(hr.getWorkDeadline());
+		 System.out.println(hr.isTodayAHoliday()); 
+		 System.out.println("==========================================================================");
+		 
+		 
+		 
+		TechDepartment tech=new TechDepartment();
+		 System.out.println(tech.departmentName());
+		 System.out.println(tech.getTodaysWork());
+		 System.out.println(tech.getWorkDeadline());
+		 System.out.println(tech.getTechStackInformation());
+		 System.out.println(tech.isTodayAHoliday());
+		
+//		System.out.println("Enter any Department from the following");
+//		System.out.println("1. Admin Deaprtment");
+//		System.out.println("2. Hr Department");
+//		System.out.println("3. Tech Department");
+//		Scanner sc=new Scanner(System.in);
+//		int No=sc.nextInt();
 		
 		
-		}
+
 		
 		
 		
